2b0096ff612134b2909b82b9b375f11ceaca99c5
gjbae1212 <gjbae1212@gmail.com>
2019-08-24 14:29:33 +0900

    Brew formula update for gossm version v1.0.4

--- gossm.rb
+++ gossm.rb
@@ -2,14 +2,14 @@
 class Gossm < Formula
   desc "gossm is interactive CLI tool that you select server in AWS and then could connect or send files your AWS server using start-session, ssh, scp under AWS Systems Manger."
   homepage ""
-  version "1.0.3"
+  version "1.0.4"
 
   if OS.mac?
-    url "https://github.com/gjbae1212/gossm/releases/download/v1.0.3/gossm_1.0.3_Darwin_x86_64.tar.gz"
-    sha256 "18c8aac65e8b3c88421cfda442e9200e426079522a82e2c6658dc545ec064a66"
+    url "https://github.com/gjbae1212/gossm/releases/download/v1.0.4/gossm_1.0.4_Darwin_x86_64.tar.gz"
+    sha256 "80240ddae1dccd5d1ece44f4827d624ae83fd22caae75f5c07eb7387ba842bee"
   elsif OS.linux?
-    url "https://github.com/gjbae1212/gossm/releases/download/v1.0.3/gossm_1.0.3_Linux_x86_64.tar.gz"
-    sha256 "a59d51c08cb2a634196142a0fef6f3479ec47c0446f3b5ba36d5a203d9f99317"
+    url "https://github.com/gjbae1212/gossm/releases/download/v1.0.4/gossm_1.0.4_Linux_x86_64.tar.gz"
+    sha256 "4fd391b1943f88868443b30dd7b3071813db9dbb6ec019cbaad63bc9ff10dab4"
   end
 
   def install
